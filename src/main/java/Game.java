06b194fe7980b260990a2eab88cc32ba41cfe013
andrefmrocha <andrefmrocha@live.com.pt>
2019-02-26 11:40:32 +0000

    Added movement to the character

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,6 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -6,9 +8,14 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
+import static com.googlecode.lanterna.input.KeyType.ArrowUp;
+
 public class Game {
-    Screen screen;
-    TextCharacter character;
+    private Screen screen;
+    private TextCharacter character;
+    private int x = 10;
+    private int y = 10;
+
 
     Game() throws IOException {
         Terminal terminal = new DefaultTerminalFactory().createTerminal();
@@ -21,11 +28,41 @@ public class Game {
 
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, this.character);
+        screen.setCharacter(x, y, this.character);
         screen.refresh();
     }
 
     public void run() throws IOException {
+        this.draw();
+        KeyStroke key;
+        do{
+            key = screen.readInput();
+            this.processKey(key);
+
+        }while (key.getKeyType() != KeyType.EOF);
+        this.processKey(key);
+    }
+
+    private void processKey(KeyStroke key) throws IOException {
+        switch (key.getKeyType())
+        {
+            case ArrowUp:
+                this.y-=1;
+                break;
+            case ArrowDown:
+                this.y+=1;
+                break;
+            case ArrowLeft:
+                this.x-=1;
+                break;
+            case ArrowRight:
+                this.x+=1;
+                break;
+            case Character:
+                if(key.getCharacter() == 'q')
+                    screen.close();
+                break;
+        }
         this.draw();
     }
 }
